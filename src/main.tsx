6e1123b7d20f8caee96a45dd4e7272798c771338
Dahnaya Joyner <dahjoyner12@gmail.com>
2026-06-15 11:52:56 -0400

    no analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { initAnalytics } from './lib/initAnalytics';
 import './index.css';
+
+initAnalytics();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
