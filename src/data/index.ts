cff99378b0356b6e3a4e7f78c25387b6b503fbd2
Dahnaya Joyner <dahjoyner12@gmail.com>
2026-06-22 17:43:57 -0400

    NYC pride

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,10 +2,12 @@ import { ENABLED_FESTIVALS } from '../constants/festivals';
 import type { PrideEvent } from '../types/event';
 import { generatedEvents as baltimorePrideEvents } from './festivals/baltimore-pride.generated';
 import { generatedEvents as june2026Events } from './festivals/june-2026.generated';
+import { generatedEvents as nycPrideEvents } from './festivals/nyc-pride.generated';
 
 const BUNDLED_BY_FESTIVAL: Record<string, PrideEvent[]> = {
   'baltimore-pride': baltimorePrideEvents,
   'june-2026': june2026Events,
+  'nyc-pride': nycPrideEvents,
 };
 
 export function bundledEventsForFestival(festivalId: string): PrideEvent[] {
